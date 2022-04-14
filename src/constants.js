807985de4b9513eff6feb92c8c45a0f9c35ce001
sirusho-grigorian <sirushogrigoryan2001@gmail.com>
2022-04-14 16:49:36 +0400

    Update verdion

--- src/constants.js
+++ src/constants.js
@@ -8,4 +8,5 @@ export const gameStatuses = {
 export const levels = [
   { id: 0, text: "Beginner", height: 10, width: 10, minesCount: 10 },
   { id: 1, text: "Intermediate", height: 15, width: 15, minesCount: 40 },
+  { id: 2, text: "Expert", height: 20, width: 20, minesCount: 60 },
 ];
